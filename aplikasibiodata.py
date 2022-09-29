b8a236be860f90f75f0f784a419fb00bb1a15881
Muhamaddafik12 <114629907+Muhamaddafik12@users.noreply.github.com>
2022-09-28 21:16:22 -0700

    Add files via upload

---
+++ aplikasibiodata.py
@@ -0,0 +1,16 @@
+# aplikasi biodata by muhamad dafik kholik firdaus
+
+#masukan nama
+print("yuk masukan nama anda")
+
+nama = input ("masukan nama anda:")
+kelas = input ("masukan kelas saat ini :")
+prodi = input ("prodi saat ini:")
+TTL = input ("tempat tanggal lahir :")
+
+print ("nama anda :", nama)
+print ("kelas saatini :", kelas)
+print ("prodi anda", prodi)
+print ("tempat tanggal lahir", TTL)
+
+
