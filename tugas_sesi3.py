0cbec16ee7d0a5838c44d2ff9c8421df5e65d1fd
Muhamaddafik12 <114629907+Muhamaddafik12@users.noreply.github.com>
2022-10-06 09:17:18 +0700

    Add files via upload

---
+++ tugas_sesi3.py
@@ -0,0 +1,11 @@
+# Biodata sederhana by Muhamad Dafik Kholik Firdaus_TI 22A
+
+nama = "Muhamad Dafik Kholik Firdaus"
+kelas = " TI22A "
+jurusan = "teknik informatika"
+quote = " jalani aja"
+
+print("nama: ", nama)
+print("kelas:", kelas)
+print("jurusan:", jurusan)
+print("quote:", quote)
